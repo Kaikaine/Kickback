8ffdbfe11c6b3514b2e9dd95a89492a783ff0177
kaikaine <kairijb@protonmail.com>
2019-02-12 10:40:58 -0600

    add friend route adds the correct person to the list now

--- routes/api/friends.js
+++ routes/api/friends.js
@@ -11,11 +11,13 @@ const validateFriendInput = require("../../validation/message");
 // find by req.body.username
 // added to list thru req.user.username
 
+// @route   POST api/friends/add/:username
+// @desc    Create message
+// @access  private
 router.post(
     "/add/:username",
     passport.authenticate("jwt", { session: false }),
     (req, res) => {
-  
       User.find({username: req.params.username})
         .then(user => {
           // console.log(user)
@@ -29,7 +31,7 @@ router.post(
                 // key == user key2 == user2
                 console.log(key.username)
                 if(key2.pending.filter(user => (user.username == key.username))) {
-                  key2.friends.push({username: req.body.username})
+                  key2.friends.push({username: req.params.username})
                   key.friends.push({username: req.user.username})
                 //   remove user from pending array
                 // save user and user2
@@ -46,7 +48,7 @@ router.post(
                 const newFriend = {
                   username: req.user.username
                 }
-                // key2.pending.push(newFriend)
+                key2.pending.push(newFriend)
                 key2.save().then(res.json(user2))
               }
               })
@@ -58,5 +60,11 @@ router.post(
         .catch(err => console.log(err));
     }
   );
+
+
+// @route   GET api/friends/events/:username
+// @desc    Get friends events
+// @access  private
+
 
 module.exports = router;
